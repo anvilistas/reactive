c3311d2e931e0649203b1a92fa83e96451dd291b
stucork@gmail.com <stucork@gmail.com>
2023-12-13 14:11:46 +0000

    Edited main._primitives

--- client_code/main/_primitives.py
+++ client_code/main/_primitives.py
@@ -187,7 +187,7 @@ class computed(ReactiveComputation):
             return self
         rcs = REACTIVE_CACHE.get(obj) or {}
         rv = rcs.get(self, None)
-        print(rv, type(rcs))
+        print(rv, rcs)
         if rv is None:
             rv = self.fn.__get__(obj, type)
             rcs[self] = rv
