3f6a23e46d6894697668a3cad0d52382a165115e
stu <stu@anvil.works>
2024-02-01 21:36:29 +0800

    Expose reactive_list/dict

--- client_code/main/__init__.py
+++ client_code/main/__init__.py
@@ -9,6 +9,8 @@ from .._internal.signal import create_effect
 from ._primitives import bind, computed, effect, render_effect, writeback
 from ._reactive_class import reactive_class, reactive_instance
 from ._signal import signal
+from ._store import ReactiveDict as reactive_dict
+from ._store import ReactiveList as reactive_list
 
 __version__ = "0.0.5"
 
