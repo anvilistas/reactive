7180d5afc32e9c12e72eb949565569e8f6393f9a
stu <stu@anvil.works>
2024-10-23 13:25:03 +0800

    fix: reactive class regression after changes to reactive dict

--- client_code/main/_reactive_class.py
+++ client_code/main/_reactive_class.py
@@ -59,7 +59,6 @@ def reactive_class(base):
         prev = None
         if attr in d:
             prev = dict_getitem(d, attr)
-            assert type(prev) is StoreSignal, "expected a signal"
         old_setattr(self, attr, val)
         if attr not in d:
             return
